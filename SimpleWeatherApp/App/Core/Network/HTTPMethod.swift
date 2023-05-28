ed4a27f20acfd495e28b4059e04052c736c306f2
onurduyar <onurdyr510@gmail.com>
2023-05-28 17:38:18 +0300

    LocalJSON network was added.

---
+++ SimpleWeatherApp/App/Core/Network/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  SimpleWeatherApp
+//
+//  Created by Onur Duyar on 23.05.2023.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
