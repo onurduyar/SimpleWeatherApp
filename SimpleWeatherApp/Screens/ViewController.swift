fd27ca51e0a3c84d459e50872df49024e7c9c8b1
onurduyar <onurdyr510@gmail.com>
2023-05-28 17:24:12 +0300

    project was initialized

---
+++ SimpleWeatherApp/Screens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by Onur Duyar on 28.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
